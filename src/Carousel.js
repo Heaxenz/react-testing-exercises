f5ba35a2cbd0286c4ba1c5d5ca5b257149d3598c
Juan Robledo <heaxenz@Rikos-Air.lan>
2023-07-27 16:19:35 -0700

    update

--- src/Carousel.js
+++ src/Carousel.js
@@ -17,42 +17,58 @@ import photos from "./photos";
  */
  function Carousel({ photos, title }) {
   const [currCardIdx, setCurrCardIdx] = useState(0);
+  const [backVisible, setbackVisible] = useState(true);
+  const [forwardVisible, setforwardVisible] = useState(true);
+  
 
   const currCard = photos[currCardIdx];
   const total = photos.length;
 
+
+
   //Increments currCardIdx state by 1
   //help hiding if on the last or first photos
   function goForward() {
-    if(currCardIdx === total -1){
-      setCurrCardIdx(currCardIdx)
+    if(currCardIdx === total -2){
+      setCurrCardIdx(currCardIdx + 1)
+      setforwardVisible(false)
+      
+
     } else setCurrCardIdx(currCardIdx + 1);
+    setbackVisible(false);
+     
   }
 
   function goBackward() {
-    if(currCardIdx === 0){
-    setCurrCardIdx(0)
-    } else setCurrCardIdx(currCardIdx - 1);
+    if(currCardIdx === 1){
+    setCurrCardIdx(0);
+    setbackVisible(true);
+    
+    } else {
+      setCurrCardIdx(currCardIdx - 1)
+      setforwardVisible(true);
+    }
   }
 
   return (
     <div className="Carousel">
       <h1>{title}</h1>
       <div className="Carousel-main">
-        <i
+        { !backVisible && <i
           className="bi bi-arrow-left-circle"
           onClick={goBackward}
-        />
+        />}
         <Card
           caption={currCard.caption}
           src={currCard.src}
           currNum={currCardIdx + 1}
           totalNum={total}
         />
+        { forwardVisible &&
         <i
           className="bi bi-arrow-right-circle"
           onClick={goForward}
-        />
+        />}
       </div>
     </div>
   );
